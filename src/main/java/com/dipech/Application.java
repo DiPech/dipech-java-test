d04d6bcbc19ae8158e85b34d207f0569918c4a41
DiPech <dmitry.pechkovsky@gmail.com>
2021-11-07 10:36:51 +0300

    Initial commit.

---
+++ src/main/java/com/dipech/Application.java
@@ -0,0 +1,12 @@
+package com.dipech;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println(String.join("\n",
+            "Houston, we DON'T have ANY problems at ALL!",
+            "Hello from https://www.dipech.com ;)"
+        ));
+    }
+
+}
